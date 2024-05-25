fa757ca137104470265a1bc1041a25ccd886fbd2
Angel <cfitz012@uottawa.ca>
2024-05-25 03:26:47 -0400

    Define interface for Marked renderer object

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,21 +1,36 @@
 import { marked } from "marked"
 import { onMount, createSignal } from "solid-js"
 
-interface filePath {
-  fileName: string;
+
+export interface renderObject {
+  code?: (codeText: string, info: string | undefined, escaped: boolean) => string;
+  paragraph?: (text: string) => string;
+  heading?: (text: string, level: number, raw?: string) => string;
+  hr?: () => string;
+  list?: (body: string, ordered: boolean, start: number | "") => string;
+  listitem?: (text: string, task: boolean, checked: boolean) => string;
+  strong?: (text: string) => string;
+  em?: (text: string) => string;
+  br?: () => string;
+  codespan?: (text: string) => string;
 }
 
-const Markdown = (props: filePath) => {
-  const [mdBody, setMdBody] = createSignal<string>("");
+interface markdownProps {
+  fileName: string;
+  renderOptions: renderObject;
+}
 
+export const Markdown = (props: markdownProps) => {
+  const [mdBody, setMdBody] = createSignal<string>("");
+  const renderer = props.renderOptions;
+  
   onMount(async () => {
     const res = await fetch(props.fileName);
     const text = await res.text();
+    marked.use({renderer});
     const htmlString = await marked.parse(text);
     setMdBody(htmlString);
   });
   
   return <div innerHTML={mdBody()}></div>
 }
-
-export default Markdown
