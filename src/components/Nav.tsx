8b38b13caed0fc4dc10aae0db482af96e463c6ce
Angel <cfitz012@uottawa.ca>
2024-05-27 21:41:40 -0400

    Update Navbar to reflect site theme/structure

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,14 +5,18 @@ export default function Nav() {
   const active = (path: string) =>
     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
   return (
-    <nav class="bg-sky-800">
+    <nav class="bg-slate-700 fixed w-screen">
       <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
+        <li class={`border-b-2 mx-1.5 sm:mx-6`}>
+          <a href="#">Home</a>
         </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
+        <li class={`border-b-2 mx-1.5 sm:mx-6 scroll-smooth`}>
+          <a href="#projects">Projects</a>
         </li>
+        <li class={`border-b-2 mx-1.5 sm:mx-6 scroll-smooth`}>
+          <a href="#tech">Technologies</a>
+        </li>
+
       </ul>
     </nav>
   );
