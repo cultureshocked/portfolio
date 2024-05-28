c62fef86195a5325c08d646f72b4dc1d13cbe6ed
Angel <cfitz012@uottawa.ca>
2024-05-28 17:22:14 -0400

    Create Footer component; Remove obsolete GHData component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <>
+    <footer class="w-screen bg-slate-800 flex p-8 justify-center text-white">
+      <div class="w-1/2 p-2">
+      </div>
+      <div class="w-1/2 p-2">
+        <p class="p-2">All logos listed on this page are the intellectual properties of their respective companies, and all logos are being used permissively for non-commercial applications.</p>
+        <p class="p-2">SolidStart and SolidJS are released under the MIT license, which can be read <a class="underline" href="https://github.com/solidjs/solid/blob/main/LICENSE">here</a>.</p>
+        <p class="p-2">I, Conrad, am not affiliated with the Rust Foundation, nor does the Rust Foundation endorse me. The use of 'Rust' trademarks on this website are permitted under the Rust Foundation's media/logo policies.</p>
+      </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
