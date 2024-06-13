6b617a01f6143b693ffd7d532c1cf79077b5fa64
Angel <cfitz012@uottawa.ca>
2024-06-13 19:45:37 -0400

    Add active section to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,20 +3,19 @@ import { useLocation } from "@solidjs/router";
 export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+    path == location.hash ? "border-sky-600" : "border-transparent hover:border-sky-600";
   return (
-    <nav class="bg-slate-700 fixed w-screen">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 mx-1.5 sm:mx-6`}>
+    <nav class="bg-slate-700 fixed w-screen flex justify-center items-center z-20">
+      <ul class="container flex items-center p-3 text-gray-200 justify-center">
+        <li class={`border-b-2 mx-1.5 sm:mx-6 ${location.hash == "" ? active("") : active("#")} transition-colors`}>
           <a href="#">Home</a>
         </li>
-        <li class={`border-b-2 mx-1.5 sm:mx-6 scroll-smooth`}>
+        <li class={`border-b-2 mx-1.5 sm:mx-6 ${active("#projects")} transition-colors`}>
           <a href="#projects">Projects</a>
         </li>
-        <li class={`border-b-2 mx-1.5 sm:mx-6 scroll-smooth`}>
+        <li class={`border-b-2 mx-1.5 sm:mx-6 ${active("#tech")} transition-colors`}>
           <a href="#tech">Technologies</a>
         </li>
-
       </ul>
     </nav>
   );
