86e25fc1bb19183071cf06ba05a4e5f385f8e002
Angel <cfitz012@uottawa.ca>
2024-06-13 19:44:35 -0400

    Create FadeIn component for animating projects + icons

---
+++ src/components/FadeIn.tsx
@@ -0,0 +1,26 @@
+import { createSignal, createEffect } from "solid-js"
+
+const FadeIn = (props) => {
+  const [visible, setVisible] = createSignal<Boolean>(false);
+  let elementRef!: HTMLDivElement;
+
+  createEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    });
+
+    observer.observe(elementRef);
+    return () => {
+      observer.disconnect();
+    }
+  });
+
+  return (
+    <div class={`transition-all duration-1000 ${visible() ? 'is-visible' : 'not-visible'}`} ref={elementRef!}>
+      {props.children}
+    </div>
+  )
+
+}
+
+export default FadeIn
