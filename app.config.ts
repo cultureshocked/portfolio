c77cec54259128bab22c990e17ac30b1d98d06d5
Angel <cfitz012@uottawa.ca>
2024-06-14 15:34:50 -0400

    Fix appconfig stash messages

--- app.config.ts
+++ app.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
   server: {
     preset: 'netlify',
   }
-=======
-    server: {
-        preset: "netlify"
-    }
->>>>>>> Stashed changes
-=======
-    server: {
-        preset: "netlify"
-    }
->>>>>>> Stashed changes
-=======
-    server: {
-        preset: "netlify"
-    }
->>>>>>> Stashed changes
 });
