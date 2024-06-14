710ceb6d95c2a13ed2d52f3e44fa727d6a4fede1
Angel <cfitz012@uottawa.ca>
2024-06-14 13:29:18 -0400

    Refactor Profile and home route to make use of new globals module

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,7 @@
 import { Markdown, renderObject } from "./Markdown"
-import { Badge, BadgeObject } from "./Badge"
+import { Badge } from "./Badge"
+import { ContactBadges, getGithubAvatarURL } from "../globals"
+import { createSignal, onMount, Show } from "solid-js"
 
 const profileRenderer: renderObject = {
   heading(text: string, level: number) {
@@ -13,12 +15,16 @@ const profileRenderer: renderObject = {
   }
 }
 
-const badges: BadgeObject[] = [
-  {text: "cultureshocked", color: "111111", logo: "github", URL: "https://github.com/cultureshocked"},
-  {text: "conradfitzg", color: "0A66C2", logo: "linkedin", URL: "https://linkedin.com/in/conradfitzg"},
-];
-
 const Profile = (props: any) => {
+  const [avatarURL, setAvatarURL] = createSignal<string>("");
+
+  console.log(avatarURL());
+
+  onMount(async () => {
+    const avatar = await getGithubAvatarURL();
+    setAvatarURL(avatar);
+  });
+
   return (
     <div class="p-10 bg-slate-100 rounded-xl max-w-5xl m-auto my-32 shadow-xl">
       <div class="flex space-between">
@@ -26,13 +32,15 @@ const Profile = (props: any) => {
           <Markdown fileName="/ABOUT.md" renderOptions={profileRenderer}/>
         </div>
         <div class="max-w-lg max-h-lg">
-          <img src={props.imgLocation} class="object-fill rounded-full" />
+          <Show when={avatarURL().length > 0} fallback={<div class="bg-gray-200 rounded-full w-max h-max"></div>}>
+            <img src={props.imgLocation} class="object-fill rounded-full" />
+          </Show>
         </div>
       </div>
       <br></br>
       Contact me via LinkedIn, GitHub, or E-Mail.
       <div class="flex">
-        {badges.map((elem) => {
+        {ContactBadges.map((elem) => {
           return <Badge badgeData={elem} />
         })}
       </div>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,41 +3,18 @@ import TechList from "../components/TechList"
 import { Icon } from "../components/TechIcon"
 import Profile from "../components/Profile"
 import Footer from "../components/Footer"
+import { ProficientIcons, FamiliarIcons } from "../globals"
 
 const Home = () => {
-
-  const proficientTechnologies: Icon[] = [
-    {name: "C", iconLocation: "./svg/c.svg"},
-    {name: "Ruby", iconLocation: "/svg/ruby.svg"},
-    {name: "gcc", iconLocation: "/svg/gnu.svg"},
-    {name: "Neovim", iconLocation: "/svg/neovim.svg"},
-    {name: "Linux", iconLocation: "/svg/linux.svg"},
-    {name: "CMake", iconLocation: "/svg/cmake.svg"},
-  ];
-
-  const familiarTechnologies: Icon[] = [
-    {name: "C++", iconLocation: "/svg/cplusplus.svg"},
-    {name: "CE", iconLocation: "/svg/compilerexplorer.svg"},
-    {name: "TypeScript", iconLocation: "/svg/typescript.svg"},
-    {name: "JavaScript", iconLocation: "/svg/javascript.svg"},
-    {name: "SolidJS", iconLocation: "/svg/solid.svg"},
-    {name: "React", iconLocation: "/svg/react.svg"},
-    {name: "Tailwind", iconLocation: "/svg/tailwindcss.svg"},
-    {name: "Python", iconLocation: "svg/python.svg"},
-    {name: "NumPy", iconLocation: "/svg/numpy.svg"},
-    {name: "Rust", iconLocation: "/svg/rust.svg"},
-    {name: "Rails", iconLocation: "/svg/rubyonrails.svg"},
-    {name: "Git", iconLocation: "/svg/git.svg"},
-  ];
 
   return <>
     <div class="h-4"></div>
     <Profile imgLocation="https://avatars.githubusercontent.com/u/59343709?v=4" />
     <ProjectList anchor={"projects"} >Here's some stuff I've built:</ProjectList>
-    <TechList anchor={"tech"} technologies={proficientTechnologies}>
+    <TechList anchor={"tech"} technologies={ProficientIcons}>
       I'm proficient with:
     </TechList>
-    <TechList technologies={familiarTechnologies}>
+    <TechList technologies={FamiliarIcons}>
       I'm familiar with:
     </TechList>
     <Footer />
