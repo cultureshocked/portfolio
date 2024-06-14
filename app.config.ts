17f71b20382500a588f5474d060ee30206d3b04a
Angel <cfitz012@uottawa.ca>
2024-06-14 15:29:35 -0400

    Merge appconfig with local

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+<<<<<<< Updated upstream
   server: {
     preset: 'netlify',
   }
+=======
+    server: {
+        preset: "netlify"
+    }
+>>>>>>> Stashed changes
 });
