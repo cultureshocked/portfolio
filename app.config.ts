60984cf2448de2c5c341f78aaeb9cf4999869a87
Angel <cfitz012@uottawa.ca>
2024-06-14 15:30:40 -0400

    Stop git complaining again

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   server: {
     preset: 'netlify',
   }
+=======
+    server: {
+        preset: "netlify"
+    }
+>>>>>>> Stashed changes
 =======
     server: {
         preset: "netlify"
